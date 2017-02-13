ea0eabfd6e9909bb07b0450ab8dbdaf570e8f05d
liu1315177581 <1315177581@qq.com>
2017-02-13 23:40:36 +0800

    新增react练习

--- reacttest/src/page/Home.js
+++ reacttest/src/page/Home.js
@@ -1,31 +1,84 @@
 import React from 'react';
 import { Link } from 'react-router';
+import formProvider from '../utils/formProvider.js';
 class Home extends React.Component{
 	constructor(){
 		super();
 		this.state={
-			name:"",
-			age:0,
-			gender:''
+			form:{
+				name:{
+					valid:false,
+					value:'a',
+					error:''
+				},
+				age:{
+					valid:false,
+					value:0,
+					error:''
+				},
+				gender:{
+					valid:false,
+					value:'',
+					error:''
+				}
+			}
 		}
 	}
-	handleValueChange(filed,value,type="string"){
+	handleValueChange(field,value,type="string"){
 		if(type==="number"){
 			value = +value;
 		}
+
+		const { form } = this.state;
+
+		const newFieldObj = { value,valid:true,error:'' };
+		switch(field){
+			case 'name':{
+				if(value.length >= 5){
+					newFieldObj.error = '用户名最多4个字符';
+					newFieldObj.valid = false;
+				}else if(value.length === 0){
+					newFieldObj.error = '请输入用户名';
+					newFieldObj.valid = false;
+				}
+				break;
+			}
+			case 'age':{
+				if(value>100 || value<=0){
+					newFieldObj.error = '请输入1~100之间的数字';
+					newFieldObj.valid = false;
+				}
+				break;
+			}
+			case 'gender':{
+				if(!value){
+					newFieldObj.error = '请选择性别';
+					newFieldObj.valid = false;
+				}
+				break;
+			}
+
+		};
 		this.setState({
-			[filed]:value
-		})
+	      form: {
+	        ...form,
+	        [field]: newFieldObj
+	      }
+	    });
 	}
 	handleSubmit(e){
 		e.preventDefault();
-		const {name,age,gender} = this.state;
+		const {form:{name,age,gender}} = this.state;
+		if(!name.valid || !age.valid || !gender.valid){
+			alert('请填写正确的信息后重试');
+			return
+		}
 		fetch('http://localhost:3000/user',{
 			method:'post',
 			body:JSON.stringify({
-				name,
-				age,
-				gender
+				name:name.value,
+				age:age.value,
+				gender:gender.value
 			}),
 			headers:{
 				'Content-Type':'application/json'
@@ -34,20 +87,16 @@ class Home extends React.Component{
 		.then((res)=>res.json())
 		.then((res)=>{
 			if(res.id){
-				console.log('添加用户成功');
-				this.setState({
-					name:'',
-					age:0,
-					gender:''
-				});
+				alert('添加用户成功')
+				
 			}else{
-				console.log('添加失败');
+				alert('添加用户失败')
 			}
 		})
 		.catch((err)=>console.error(err))
 	}
 	render(){
-		const {name,age,gender} = this.state;
+		const {form:{name,age,gender}} = this.state;
 		return(
 			<div>
 				<header>
@@ -56,17 +105,20 @@ class Home extends React.Component{
 				<main>
           			<form onSubmit={(e)=>this.handleSubmit(e)}>
           				<label>用户名：</label>
-          				<input type="text" value={name} onChange={(e)=>this.handleValueChange('name',e.target.value)}/>
+          				<input type="text" value={name.value} onChange={(e)=>this.handleValueChange('name',e.target.value)}/>
+          				{!name.valid && <span>{name.error}</span>}
           				<br/>
           				<label>年龄：</label>
-          				<input type="number" value={age || ''} onChange={(e)=>this.handleValueChange('age',e.target.value,'number')}/>
+          				<input type="number" value={age.value || ''} onChange={(e)=>this.handleValueChange('age',e.target.value,'number')}/>
+          				{!age.valid && <span>{age.error}</span>}
           				<br/>
           				<label>性别：</label>
-          				<select value={gender} onChange={(e)=>this.handleValueChange('gender',e.target.value)}>
+          				<select value={gender.value} onChange={(e)=>this.handleValueChange('gender',e.target.value)}>
           					<option value="">请选择</option>
           					<option value="male">男</option>
           					<option value="female">女</option>
           				</select>
+          				{!gender.valid && <span>{gender.error}</span>}
           				<br/>
           				<input type="submit" value="提交"/>
           			</form>
---
+++ reacttest1/src/page/Home.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import formProvider from '../utils/formProvider';
+
+class Home extends React.Component {
+  handleSubmit (e) {
+    e.preventDefault();
+
+    const {form: {name, age, gender}, formValid} = this.props;
+    //console.log(formValid)
+    if (!formValid) {
+      alert('请填写正确的信息后重试');
+      return;
+    }
+
+    fetch('http://localhost:3000/user', {
+      method: 'post',
+      body: JSON.stringify({
+        name: name.value,
+        age: age.value,
+        gender: gender.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.id) {
+          alert('添加用户成功');
+          this.setState({
+            name: '',
+            age: 0,
+            gender: ''
+          });
+        } else {
+          alert('添加失败');
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+  render () {
+    const {form: {name, age, gender}, onFormChange} = this.props;
+    return (
+      <div>
+        <header>
+          <h1>添加用户</h1>
+        </header>
+
+        <main>
+          <form onSubmit={(e) => this.handleSubmit(e)}>
+            <label>用户名：</label>
+            <input
+              type="text"
+              value={name.value}
+              onChange={(e) => onFormChange('name', e.target.value)}
+            />
+            {!name.valid && <span>{name.error}</span>}
+            <br/>
+            <label>年龄：</label>
+            <input
+              type="number"
+              value={age.value || ''}
+              onChange={(e) => onFormChange('age', +e.target.value)}
+            />
+            {!age.valid && <span>{age.error}</span>}
+            <br/>
+            <label>性别：</label>
+            <select
+              value={gender.value}
+              onChange={(e) => onFormChange('gender', e.target.value)}
+            >
+              <option value="">请选择</option>
+              <option value="male">男</option>
+              <option value="female">女</option>
+            </select>
+            {!gender.valid && <span>{gender.error}</span>}
+            <br/>
+            <br/>
+            <input type="submit" value="提交"/>
+          </form>
+        </main>
+      </div>
+    );
+  }
+}
+
+Home = formProvider({
+  name: {
+    defaultValue: '',
+    rules: [
+      {
+        pattern: function (value) {
+          return value.length > 0;
+        },
+        error: '请输入用户名'
+      },
+      {
+        pattern: /^.{1,4}$/,
+        error: '用户名最多4个字符'
+      }
+    ]
+  },
+  age: {
+    defaultValue: 0,
+    rules: [
+      {
+        pattern: function (value) {
+          return value >= 1 && value <= 100;
+        },
+        error: '请输入1~100的年龄'
+      }
+    ]
+  },
+  gender: {
+    defaultValue: '',
+    rules: [
+      {
+        pattern: function (value) {
+          return !!value;
+        },
+        error: '请选择性别'
+      }
+    ]
+  }
+})(Home);
+
+export default Home;
